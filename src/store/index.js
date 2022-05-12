c0813cde2edbf61f86c3d3a8b807ff4dd73d90d0
androidm16 <mkhosanasiyabonga@gmail.com>
2022-05-12 14:28:05 +0200

    installed axios and vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,24 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
+import axios from 'axios';
 
 export default createStore({
   state: {
+    todos: []
   },
   getters: {
+    allTodos: (state) => state.todos
   },
   mutations: {
+    setTodos: (state, todos) => (state.todos = todos)
   },
   actions: {
+    async fetchTodos({ commit }) {
+      const response = await axios.get('https://to-do-backend-app1.herokuapp.com/api/task');
+
+      commit('setTodos', response.data);
+    }
   },
   modules: {
+    
   }
 })
